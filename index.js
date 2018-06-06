ff8b18f324b2d80a1f677e1943b4458a5f3b61b2
astur <astur@yandex.ru>
2018-06-06 22:23:44 +0300

    refactored after linting

--- index.js
+++ index.js
@@ -1,82 +1,54 @@
-module.exports = function(optstring, args){
+module.exports = (optstring, args = process.argv.slice(2)) => {
+    if(!Array.isArray(args)) throw new Error('arguments must be an Array');
+    if(!args.every(a => typeof a === 'string')) throw new Error('every argument must be a String');
 
-    if (typeof optstring !== 'string') {
-        throw new Error('optstring must be a String');
-    }
+    if(typeof optstring !== 'string') throw new Error('optstring must be a String');
+    if(/[^a-zA-Z0-9:]/.test(optstring)) throw new Error('only alnums and colons allowed in optstring');
+    if(optstring[0] === ':') throw new Error('first char of optstring must be alnum');
+    if(/W/.test(optstring)) throw new Error('option "-W" is resulterved');
+    if(/::/.test(optstring)) throw new Error('double colons are not allowed');
+    if(/([a-zA-Z0-9])(.*)\1/.test(optstring)) throw new Error('repeating options are not allowed');
 
-    if (/[^a-zA-Z0-9:]/.test(optstring)) {
-        throw new Error('only alnums and colons allowed in optstring');
-    }
+    const result = {};
 
-    if (optstring[0] === ':') {
-        throw new Error('first char of optstring must be alnum');
-    }
-
-    if (/W/.test(optstring)) {
-        throw new Error('option "-W" is reserved');
-    }
-
-    if (/::/.test(optstring)) {
-        throw new Error('double colons are not allowed');
-    }
-
-    if (/([a-zA-Z0-9])(.*)\1/.test(optstring)) {
-        throw new Error('repeating options are not allowed');
-    }
-
-    if (args === undefined) {
-        args = process.argv.slice(2);
-    } else if (!Array.isArray(args)) {
-        throw new Error();
-    }
-
-    var res = {},
-        i, l, a, c;
-
-    for (i = 0, l = optstring.length; i < l; i++) {
-        if (optstring[i + 1] === ':') {
-            res[optstring[i]] = '';
+    for(let i = 0, l = optstring.length; i < l; i++){
+        if(optstring[i + 1] === ':'){
+            result[optstring[i]] = '';
             i++;
         } else {
-            res[optstring[i]] = false;
+            result[optstring[i]] = false;
         }
     }
 
-    for (i = 0, l = args.length; i < l; i++) {
-        a = args[i];
-        if (a === '--') {
-            res._ = args.slice(i+1);
-            return res;
+    for(let i = 0, l = args.length; i < l; i++){
+        if(args[i] === '--'){
+            result._ = args.slice(i + 1);
+            return result;
         }
 
-        if (!/^-/.test(a) || /\s/.test(a)) {
-            res._ = args.slice(i);
-            return res;
+        if(!/^-/.test(args[i]) || /\s/.test(args[i])){
+            result._ = args.slice(i);
+            return result;
         }
 
-        a = args[i].split('').slice(1);
-        while ( ( c = a.shift() ) !== undefined ) {
-            switch (typeof res[c]) {
-            case 'undefined':
-                throw new Error('illegal option -- ' + c);
-            case 'boolean':
-                res[c] = true;
-                continue;
-            case 'string':
-                if (a.length > 0) {
-                    res[c] = a.join('');
-                    a = [];
+        const opts = args[i].split('').slice(1);
+        let c;
+        while((c = opts.shift()) !== undefined){
+            if(typeof result[c] === 'undefined') throw new Error(`illegal option -- ${c}`);
+            if(typeof result[c] === 'boolean') result[c] = true;
+            if(typeof result[c] === 'string'){
+                if(opts.length > 0){
+                    result[c] = opts.join('');
                     break;
-                } else if (args[i + 1] !== undefined) {
-                    res[c] = args[i + 1];
-                    i++;
-                    break;
+                } else if(args[i + 1] === undefined){
+                    throw new Error(`option ${c} needs argument`);
                 } else {
-                    throw new Error('option ' + c + ' needs argument');
+                    result[c] = args[i + 1];
+                    i++;
                 }
             }
         }
     }
 
-    return res;
+    return result;
 };
